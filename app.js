97f51b6e6a5699cd4e6b339068390475728b70ed
EddieMJr <millsapsjr2003@gmail.com>
2025-09-09 21:34:22 -0400

    changed some more things on the outer most app.js file

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const movies = require('./movieServer/src/movies.js')
+// grabs export from search and similar js pages & imports them here
+const search = require('./utils/search.js')
+const similar = require('./utils/similar.js')
 
-console.log('Starting')
+const movie = process.argv[2]
+
+// all code to catch errors that may come through when finding location
+if (!movie) {
+    console.log('please provide a movie')
+} else {
+    search(movie, (error, {lat, long, location} = {}) => {
+    if (error) {
+        return console.log(error)
+    }
+
+    similar(lat, long, (error, forecastData) => {
+        if (error) {
+        return console.log(error)
+    }
+        console.log(location)
+        console.log(forecastData)
+    })
+})
+}
+
+console.log(process.argv)
